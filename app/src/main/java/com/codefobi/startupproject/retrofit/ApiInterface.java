bf7f14ee3a5385688b97694749a9d7c35225b77d
CodeFobi <CodeFobi>
2016-11-08 20:18:45 +0330

    importing retrofit

--- app/src/main/java/com/codefobi/startupproject/retrofit/ApiInterface.java
+++ app/src/main/java/com/codefobi/startupproject/retrofit/ApiInterface.java
@@ -1,10 +1,22 @@
 package com.codefobi.startupproject.retrofit;
 
+import com.codefobi.startupproject.models.Content;
+import com.codefobi.startupproject.models.ReadContent;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by tosantechnolocal on 11/8/2016.
  */
-public class ApiInterface {
+public abstract class ApiInterface {
 
-    
+    @GET("/amin/content.json")
+    abstract Call<List<Content>> contentCall();
+
+    @GET("/amin/readcontent.json")
+    abstract Call<List<ReadContent>> readContentCall();
 
 }
